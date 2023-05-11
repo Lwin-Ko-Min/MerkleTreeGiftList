4ba2493a7678a3a412d4d4ef1f68a05d82390d37
Lwin-Ko-Min <129581383+Lwin-Ko-Min@users.noreply.github.com>
2023-05-11 22:46:24 +0630

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list? 
+  const tree = new MerkleTree(niceList);
+  const root = tree.getRoot();
+  const [name] = "Norman Block"
+  const index = niceList.findIndex(n => n === name);
+  const proof = tree.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    name: name,
+    root: root,
+    proof: proof,
+  });
+
+  console.log({ gift });
+ // console.log(proof);
+}
+
+main();
